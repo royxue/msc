3c822c547dd99095e9e3e3a487b434bd9738005f
Roy Xue <xljroy@gmail.com>
2020-02-16 15:22:05 +0800

    Multiple fixes

---
+++ src/components/footer_block.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterBlock = () => {
+  return (
+    <div className="footer-block">
+      <div className="footer-left">
+        <p className="footer-btn">参与共创</p>
+      </div>
+      <div className="footer-right">
+        <p className="footer-btn">欢迎分享</p>
+      </div>
+    </div>
+  )
+};
+
+export default FooterBlock;
--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "banner.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "banner.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
