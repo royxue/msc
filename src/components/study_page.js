83c137d9809d8b139b0f3652a26e7da4c89474f8
xichen0415 <chenxi@fiture.com>
2020-02-16 16:03:01 +0800

    update

--- src/components/study_page.js
+++ src/components/study_page.js
@@ -6,38 +6,35 @@ import {
     MediaBoxTitle,
     MediaBoxDescription
   } from "react-weui";
+import _ from "lodash";
+import ColBox from "../components/col_box";
 
+const DATA= {
+  info:[
+    {
+      title:"战略二",
+      description:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+    },
+    {
+      title:"",
+      description:""
+    }
+  ]
+}
 class StudyPage extends React.Component {
   render() {
+    const contentboxStyle = {
+      "-webkit-line-clamp": 'none'
+    }
+    
+    var boxList = _.map(DATA['info'], (info) => {
+      return <ColBox title={info.title} content = {info.description} />
+    })
+
     return(
       <Panel style={{display: this.props.display}}>
         <PanelBody>
-          <MediaBox type="text">
-            <MediaBoxTitle>战略一</MediaBoxTitle>
-            <MediaBoxDescription>
-              第一问
-            </MediaBoxDescription>
-          </MediaBox>
-          <MediaBox type="text">
-            <MediaBoxTitle>战略二</MediaBoxTitle>
-            <MediaBoxDescription>
-              <div style={{"-webkit-line-clamp":'none'}} >
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-            </MediaBoxDescription>
-          </MediaBox>
-          <MediaBox type="text">
-            <MediaBoxTitle>战略三</MediaBoxTitle>
-            <MediaBoxDescription>
-              第三问
-            </MediaBoxDescription>
-          </MediaBox>
-          <MediaBox type="text">
-            <MediaBoxTitle>战略四</MediaBoxTitle>
-            <MediaBoxDescription>
-              第四问
-            </MediaBoxDescription>
-          </MediaBox>
+          {boxList}
         </PanelBody>
       </Panel>
     )
