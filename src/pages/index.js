e2b709dfe2db8da6cacb45a6f399e9a40fe01a46
XiChen0415 <xi@fiture.com>
2020-03-05 17:44:01 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,19 +20,6 @@ import './index.css'
 
 import banner from '../images/banner.png';
 import share from '../images/share.png';
-<<<<<<< HEAD
-import axios from 'axios';
-
-const token_config={
-  "token":"",
-  "appid":"wx0a85b83326fb21bf",
-  "appsecret": "a1d2d4fb2f4dbb6f2fe4f33e16e02b35",
-  "getJsapiTicket":"https://api.weixin.qq.com/cgi-bin/ticket/getticket",
-  "getAccessToken":"https://api.weixin.qq.com/cgi-bin/token"
-};
-const fetchUrl = token_config.getAccessToken+'?grant_type=client_credential&appid='+token_config.appid+'&secret='+token_config.appsecret;
-=======
->>>>>>> 376ee95c2d7fddf15684403b0eba0b349acf8fbd
 
 class IndexPage extends React.Component {
   constructor(props){
@@ -93,11 +80,7 @@ class IndexPage extends React.Component {
             'mask_image_visible':this.state.mask
           })}>
           <img
-<<<<<<< HEAD
               src={share} style={{height:'75%', width:'100%'}}
-=======
-              src={share} style={{height:'75%', width:'75%'}}
->>>>>>> 376ee95c2d7fddf15684403b0eba0b349acf8fbd
           /></div></div>
           <img id='banner' src={banner}
             style={{position: 'flex', width:'100%', margin: '0 0 -0.5rem 0'}}
